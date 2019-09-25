c9ca28e64d9c14405399f38cdacc98f5e768cc6e
Jayanth771 <55347687+Jayanth771@users.noreply.github.com>
2019-09-26 00:40:54 +0530

    Update Find the Median .c

--- hackerrank/problem solving/Find the Median .c
+++ hackerrank/problem solving/Find the Median .c
@@ -8,21 +8,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-void med(long long int a[],long int n){
-    long long int i,j,temp;
-    for(i=0;i<n;i++){
+void swap(long long int* a, long long int* b) 
+{ 
+    long long int t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+int partition (long long int arr[], long long int low, long long int high) 
+{ 
+    long long int pivot = arr[high];    
+    long long int i = (low - 1);   
+  
+    for (long long int j = low; j <= high- 1; j++) 
+    { 
         
-        for(j=0;j<n-1-i;j++){
-            if(a[j]>a[j+1]){
-                
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-                
-            }
-        }
+        if (arr[j] < pivot) 
+        { 
+            i++;    
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+
+void quickSort(long long int arr[], long long int low, long long int high) 
+{ 
+    if (low < high) 
+    { 
         
+        long long int pi = partition(arr, low, high); 
+  
+    
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
     }
+   
+}
+
+
+
+int main(){
+    long long int n;
+    scanf("%ld",&n);
+    
+    long long int a[n];
+    long long int i;
+ 
+    for(i=0;i<n;i++){
+        scanf("%lld,",&a[i]);
+    }
+   quickSort(a, 0, n-1);
     long long int median;
     if(n%2==0){
         median=(a[n/2]+a[(n/2)-1])/2;
@@ -30,25 +67,6 @@ void med(long long int a[],long int n){
     }else{
         median=a[((n+1)/2)-1];
     }printf("%lld",median);
-    
-    
-}
-
-
-
-
-int main(){
-    long int n;
-    scanf("%ld",&n);
-    
-    long long int a[n];
-    long int i;
-    //loop for taking input
-    for(i=0;i<n;i++){
-        scanf("%lld,",&a[i]);
-    }
-    med(a,n);
-    
     
     return 0;
 }
