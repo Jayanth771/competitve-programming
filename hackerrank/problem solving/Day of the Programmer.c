5ff46235fcd490a04f47ef8794ca0cd4e04d8c8f
Jayanth771 <55347687+Jayanth771@users.noreply.github.com>
2019-10-05 17:51:27 +0530

    Create Day of the Programmer.c

---
+++ hackerrank/problem solving/Day of the Programmer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void day_of_programmer(int year){
+    if (year == 1918){
+        printf("26.09.1918");
+    }
+    else if (((year <= 1917) && (year%4 == 0)) || ((year%400 == 0) || ((year%4 ==0) && (year%100 != 0)))){
+        printf("12.09.%d",year);
+     } else{
+        printf("13.09.%d",year);
+}
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    day_of_programmer(n);
+}
