7b73a4a9b7aaebd4ea80a571e8ecd5f49d8082f3
Jayanth771 <55347687+Jayanth771@users.noreply.github.com>
2019-10-07 18:22:54 +0530

    Create Cut the sticks.c

---
+++ hackerrank/problem solving/Cut the sticks.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void swap(int* a, int* b)
+{
+int t = *a;
+*a = *b;
+*b = t;
+}
+int partition (int arr[], int low, int high)
+{
+int pivot = arr[high];
+int i = (low - 1);
+for (int j = low; j <= high- 1; j++)
+{
+if (arr[j] <= pivot)
+{
+i++;
+swap(&arr[i], &arr[j]);
+}
+}
+swap(&arr[i + 1], &arr[high]);
+return (i + 1);
+}
+void quickSort(int arr[], int low, int high)
+{
+if (low < high)
+{
+int pi = partition(arr, low, high);
+quickSort(arr, low, pi - 1);
+quickSort(arr, pi + 1, high);
+}
+}
+int min(int arr[], int n){
+    int min;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            min=arr[i];
+            break;
+        }
+    }
+    return min;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+int sum;
+    quickSort(arr,0,n-1);
+    while(arr[n-1]!=0){
+        sum=0;
+       int m;
+       m=min(arr,n);
+    for(int i=0;i<n;i++){
+        if(arr[i]-m>=0){
+        sum++;
+        arr[i]-=m;
+        }
+    
+ 
+    }
+    printf("%d\n",sum);
+    }
+}
