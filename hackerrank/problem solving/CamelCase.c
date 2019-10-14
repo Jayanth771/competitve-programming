d3458bc50579d217774e87a0259526fba9610137
Pranjal Walia <52863441+masterchief01@users.noreply.github.com>
2019-10-15 00:49:22 +0530

    Update CamelCase.c

--- hackerrank/problem solving/CamelCase.c
+++ hackerrank/problem solving/CamelCase.c
@@ -10,10 +10,12 @@
 
 int main(){
     char *str;
-    str=(char*)malloc(1*sizeof(char));
+    str=(char*)malloc(1*sizeof(char)); 
     scanf("%s",str);
     long long int n=strlen(str);
     long long int count=1;
+    // a better way would be just to check if the condition str[i] < 97, more memory efficient
+    
     for(long long int i=0;i<n;i++){
         if(str[i]>='A' && str[i]<='Z'){
             count++;
